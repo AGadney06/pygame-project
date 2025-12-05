74a86e67bb769d388dfcd194fa0e3b247a3265c9
opacyno <oliver21112006@gmail.com>
2025-12-05 13:36:38 +0000

    Add files via upload

---
+++ Project/player.py
@@ -0,0 +1,55 @@
+import pygame #imports pygame
+from laser import Laser #Imports the Laser class from laser.py
+
+class Player(pygame.sprite.Sprite): #Player class for the player sprite
+    def __init__(self,pos,restriction,speed): #Constructor
+        super().__init__() #Calls the constructor to inherit its attributes
+        self.image = pygame.image.load('defender.png') #loads the defender.png file
+        self.image = pygame.transform.scale(self.image, (50,50)) #Resizes the player sprite to a more appropriate size
+        self.rect = self.image.get_rect(midbottom = pos)
+        self.speed = speed #Speed of player
+        self.max_x_restriction = restriction #The x coordinate boundaries of the game
+        self.ready = True
+        self.laster_time = 0
+        self.laser_cooldown = 600
+
+        self.lasers = pygame.sprite.Group()
+        self.laser_sound = pygame.mixer.Sound('laser.mp3')
+        self.laser_sound.set_volume(0.5)
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]: #If the right key is pressed move the player to the right
+            self.rect.x += self.speed
+        elif keys[pygame.K_LEFT]: #If the left key is pressed move the player to the left
+            self.rect.x -= self.speed
+
+        if keys[pygame.K_SPACE] and self.ready:
+            self.shoot_laser()
+            self.ready = False
+            self.laser_time = pygame.time.get_ticks() #Checks the timer once
+            self.laser_sound.play()
+
+    def recharge(self):
+        if not self.ready:
+            current_time = pygame.time.get_ticks() #Checks the timer continuously until the cooldown reaches its maximum 
+            if current_time - self.laser_time >= self.laser_cooldown: #Checks to see if the timer has reached the end of the cooldown period
+                self.ready = True #The laser is now ready again to fire
+    
+    def restriction(self):
+        if self.rect.left <= 0:
+            self.rect.left = 0
+
+        if self.rect.right >= self.max_x_restriction:
+            self.rect.right = self.max_x_restriction
+
+    def shoot_laser(self): #shoot laser function
+        self.lasers.add(Laser(self.rect.center, -8,self.rect.bottom)) #Whenever laser is below player, destroy it
+
+    def update(self): #Update function
+        self.get_input() #Calls player inputs
+        self.restriction() #Calls boundaries of the game
+        self.recharge() #Calls the laser's recharge mechanic
+        self.lasers.update() 
+    
