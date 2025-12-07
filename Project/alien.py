12805075b3bd30da0c2fb00652ddb14c709adb8f
opacyno <oliver21112006@gmail.com>
2025-12-07 16:45:07 +0000

    Add files via upload

---
+++ Project/alien.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Alien(pygame.sprite.Sprite):
+    def __init__(self,type,x,y,speed):
+        super().__init__() #Calls the constructor to inherit its attributes
+        file_path = '../Project/' +  type + '.png'
+        self.image = pygame.image.load(file_path).convert_alpha() #Loads alien image
+        self.image = pygame.transform.scale(self.image, (50,50)) #Rescales alien image
+        self.rect = self.image.get_rect(topleft = (x,y)) #Sets the rectangle for the alien
+        self.speed = speed
+
+    def update(self,direction,alien_speed):
+        self.speed = alien_speed
+        self.rect.x += direction * self.speed
